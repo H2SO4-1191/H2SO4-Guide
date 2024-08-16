63cfcedb053269c18b2386298915aea3e576ff84
H2SO4-1191 <H2SO4.1191@GMAIL.COM>
2024-08-16 13:12:09 +0300

    Initialization

---
+++ app/src/main/java/com/h2so4/h2so4_guide/controller/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.h2so4.h2so4_guide.controller
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.h2so4.h2so4_guide.R
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+    }
+}
